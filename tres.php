268e9d54eb12fa24837b2789fca366968631cd09
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 09:55:11 +0100

    Update tres.php and um.php

--- tres.php
+++ tres.php
@@ -2,9 +2,8 @@
 $txt1 = " Eu sou o Joao e";
 $txt2 = " na turma sou o numero";
 $txt3 = " Vou fazer este modulo com";
+$txt4 = " Ricardo Fernandes nº9 12i.2 ";
 $num1 = " 15";
 $num2 = " 20";
-echo $txt1. "" .$txt2. "" .$num1. "." .$txt3."".$num2. " Valores";
-echo "<br>";
-echo "<br>";
+echo $txt1. "" .$txt2. "" .$num1. "." .$txt3."" .$txt4."".$num2. " Valores";
 ?> 
